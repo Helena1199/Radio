07b451cf5c1620f00dbd6165a4c0a272bce312fa
Helena <v0robjeva.alena@yandex.ru>
2023-01-09 15:03:09 +0300

    Radio1

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -1,2 +1,66 @@
 public class Radio {
+    private int currentStation;
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int newCurrentStation) {
+        if (newCurrentStation < 0) {
+            return;
+        }
+        if (newCurrentStation > 9) {
+            return;
+        }
+        currentStation = newCurrentStation;
+    }
+
+    public void nextStation() {
+
+        if (currentStation < 9) {
+            currentStation++;
+        } else {
+            currentStation = 0;
+        }
+    }
+
+    public void prevStation() {
+        if (currentStation != 0) {
+            currentStation--;
+        } else {
+            currentStation = 9;
+        }
+    }
+
+
+    private int currentVolume;
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume (int newCurrentVolume) {
+        if (newCurrentVolume < 0) {
+            return;
+        }
+        if (newCurrentVolume > 10) {
+            return;
+        }
+        currentVolume = newCurrentVolume;
+    }
+
+
+
+    public void increaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume ++;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume != 0) {
+            currentVolume--;
+        } else {
+            currentVolume = 0;
+        }
+    }
 }
